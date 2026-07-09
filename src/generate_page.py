73a4a6200cdfd1e31ae13de45536a9c55b760a7b
sid535 <115077155+Sid535@users.noreply.github.com>
2026-07-09 21:33:02 +0530

    built deployment ready

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,7 +12,7 @@ def extract_title(markdown: str) -> str:
             return ""
     raise ValueError("No h1 header found in markdown")
     
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str | None = "/") -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path) as f:
@@ -26,6 +26,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     
     template_content = template_content.replace(r"{{ Title }}", html_title, 1)
     template_content = template_content.replace(r"{{ Content }}", html_content, 1)
+    template_content = template_content.replace('href="/', f'href="{basepath}')
+    template_content = template_content.replace('src="/', f'src="{basepath}')
     
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_path):
@@ -33,7 +35,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     with open(dest_path, "w") as f:
         f.write(template_content)
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str | None = "/") -> None:
     for item in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, item)
         dst_path = os.path.join(dest_dir_path, item)
@@ -42,7 +44,7 @@ def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir
             if src_path.endswith(".md"):
                 dst_path = dst_path[:-2]
                 dst_path += "html"
-                generate_page(src_path, template_path, dst_path)
+                generate_page(src_path, template_path, dst_path, basepath)
         elif os.path.isdir(src_path):
             os.makedirs(dst_path, exist_ok=True)
-            generate_pages_recursive(src_path, template_path, dst_path)
+            generate_pages_recursive(src_path, template_path, dst_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
-import os, shutil
+import os, shutil, sys
 from generate_page import generate_pages_recursive
 
 
-def copy_static_to_public() -> None:
-    src_dir = '/home/siddesh/myprojects/Github/static_site_python/static'
-    dst_dir = '/home/siddesh/myprojects/Github/static_site_python/public'
-    if os.path.exists(dst_dir):
-        for filename in os.listdir(dst_dir):
-            file_path = os.path.join(dst_dir, filename)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(script_dir)  
+
+
+def copy_static_to_public(static_dir: str, docs_dir: str) -> None:
+    if os.path.exists(docs_dir):
+        for filename in os.listdir(docs_dir):
+            file_path = os.path.join(docs_dir, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.unlink(file_path)
@@ -16,30 +18,35 @@ def copy_static_to_public() -> None:
             except Exception as e:
                 raise Exception('Failed to delete %s. Reason: %s' % (file_path, e))
     else:
-        os.makedirs(dst_dir)
+        os.makedirs(docs_dir)
         
-    copy_recursive(src_dir, dst_dir)
+    copy_recursive(static_dir, docs_dir)
     
-def copy_recursive(src_dir: str, dst_dir: str) -> None:
-    for item in os.listdir(src_dir):
-        src_path = os.path.join(src_dir, item)
-        dst_path = os.path.join(dst_dir, item)
+def copy_recursive(static_dir: str, docs_dir: str) -> None:
+    for item in os.listdir(static_dir):
+        static_path = os.path.join(static_dir, item)
+        docs_path = os.path.join(docs_dir, item)
         
-        if os.path.isfile(src_path):
-            shutil.copy(src_path, dst_path) 
-        elif os.path.isdir(src_path):
-            os.makedirs(dst_path, exist_ok=True)
-            copy_recursive(src_path, dst_path)
+        if os.path.isfile(static_path):
+            shutil.copy(static_path, docs_path) 
+        elif os.path.isdir(static_path):
+            os.makedirs(docs_path, exist_ok=True)
+            copy_recursive(static_path, docs_path)
             
 
 def main() -> None:
-    copy_static_to_public()
+    static_dir = os.path.join(project_root, "static")
+    content_dir = os.path.join(project_root, "content")
+    template_path = os.path.join(project_root, "template.html")
+    docs_dir = os.path.join(project_root, "docs")
     
-    src_dir = '/home/siddesh/myprojects/Github/static_site_python/content/'
-    template_path = '/home/siddesh/myprojects/Github/static_site_python/template.html'
-    dst_dir = '/home/siddesh/myprojects/Github/static_site_python/public/'
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
     
-    generate_pages_recursive(src_dir, template_path, dst_dir)
+    copy_static_to_public(static_dir, docs_dir)
+    generate_pages_recursive(content_dir, template_path, docs_dir, basepath)
     
     
 if __name__ == '__main__':
